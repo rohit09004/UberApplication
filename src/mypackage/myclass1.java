9482a93b44e9152a3bd85de31bde8b1dac97cd15
ROHIT <ROHIT@DESKTOP-8SI0GBB>
2022-08-23 00:44:02 +0530

    stag changes

---
+++ src/mypackage/myclass1.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class myclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Uber Application Verification 1");
+		System.out.println("Uber Application Verification 2");
+	}
+
+}
